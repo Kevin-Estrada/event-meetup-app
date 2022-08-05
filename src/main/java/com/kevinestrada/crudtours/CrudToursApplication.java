0f624990bdf688f1c002fbef318eb75cc04534b2
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2022-08-04 20:39:49 -0500

    Adding the first commit with the basic spring boot configuration

---
+++ src/main/java/com/kevinestrada/crudtours/CrudToursApplication.java
@@ -0,0 +1,13 @@
+package com.kevinestrada.crudtours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudToursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudToursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinestrada/crudtours/CrudToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinestrada.crudtours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudToursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
